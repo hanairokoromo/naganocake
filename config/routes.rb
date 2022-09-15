550b2f481e7516220cf25a382971d7e812d2e992
EC2 Default User <ec2-user@ip-172-31-27-191.ap-northeast-1.compute.internal>
2022-09-15 17:47:03 +0000

    【Update】会員一覧・詳細画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   root to: "admin/homes#top"
   
   namespace :public do
@@ -10,6 +11,7 @@ Rails.application.routes.draw do
     root to: "admin/homes#top"
     resources :genres, only: [:index, :create, :edit, :update]
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
   end
   
   devise_for :admins, skip: [:registrations, :passwords], controllers: {
