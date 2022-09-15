397da400a0402293c26abc5e8972efd7af1c31ee
hanairokoromo <tneri.0mc5.rin2do2@gmail.com>
2022-09-15 17:47:03 +0000

    【Update】会員一覧・詳細画面の作成

---
+++ app/controllers/admin/customers_controller.rb
@@ -0,0 +1,18 @@
+class Admin::CustomersController < ApplicationController
+  def index
+    @customers = Customer.all
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+  end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number,:is_deleted)
+  end
+end
