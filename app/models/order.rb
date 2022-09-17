9264ad11c22afd241e012097dd527df23f85b660
hanairokoromo <tneri.0mc5.rin2do2@gmail.com>
2022-09-17 19:08:25 +0000

    【Update】enumの導入

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,8 @@
 class Order < ApplicationRecord
+    enum payment_method: {credit_card: 0, transfer: 1}
+    enum status: {waiting: 0, confirm: 1, production: 2, preparing: 3, shipped: 4}
+    has_one_attached :image
+    belongs_to :customer
+    has_many :order_details, dependent: :destroy
+    
 end
