694391187d84133ad719098fb9dd7a0d4de5207b
hanairokoromo <tneri.0mc5.rin2do2@gmail.com>
2022-09-28 15:38:11 +0000

    【Update】カート画面の作成

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,4 +2,12 @@ class CartItem < ApplicationRecord
     has_one_attached :image
     belongs_to :customer
     belongs_to :item
+    
+    def with_tax_price
+        (price * 1.1).floor
+    end
+    
+    def subtotal
+        item.with_tax_price * amount
+    end
 end
