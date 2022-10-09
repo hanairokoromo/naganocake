6aceecf64e7f72f1c8ec1461d9ee6d3806c016ed
EC2 Default User <ec2-user@ip-172-31-27-191.ap-northeast-1.compute.internal>
2022-10-09 16:58:16 +0000

    【Update】注文確認画面の作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,6 +5,19 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+    @cart_items = current_customer.cart_items
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    if params[:order][:address_id] == "1"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.first_name + current_customer.last_name
+    elsif params[:order][:address_id] == "2"
+      @order.postal_code = Address.find(params[:order][:address_id]).postal_code
+      @order.address = Address.find(params[:order][:address_id]).address
+      @order.name = Address.find(params[:order][:address_id]).name
+    end
+    @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
   end
 
   def complete
@@ -14,5 +27,11 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
+  end
+  
+  private
+  
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
   end
 end
