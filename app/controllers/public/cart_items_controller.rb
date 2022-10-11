12c38fd979e55196a3779a661e4730efa1621e14
EC2 Default User <ec2-user@ip-172-31-27-191.ap-northeast-1.compute.internal>
2022-10-11 17:34:33 +0000

    【Update】レイアウト調整

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,6 +1,7 @@
 class Public::CartItemsController < ApplicationController
   def index
     @cart_items = current_customer.cart_items
+    @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
   end
   
   def update
@@ -12,8 +13,7 @@ class Public::CartItemsController < ApplicationController
   
   def destroy
     @cart_item = CartItem.find(params[:id])
-    @cart_item.destroy(cart_item_params)
-    @total = total_price(@cart_items).to_s(:delimited)
+    @cart_item.destroy
     redirect_to public_cart_items_path
   end
   
@@ -24,15 +24,21 @@ class Public::CartItemsController < ApplicationController
   end
   
   def create
-    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
       @cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
       params[:cart_item][:amout] = params[:cart_item][:amount].to_i + @cart_item.amount
       @cart_item.update(cart_item_params)
       redirect_to public_cart_items_path
     else
       @cart_item = CartItem.new(cart_item_params)
-      @cart_item.save
-      redirect_to public_cart_items_path
+      @cart_item.customer_id = current_customer.id
+      if @cart_item.save
+        redirect_to public_cart_items_path
+      else
+        @item = @cart_item.item
+        flash[:error] = "個数を入力してください"
+        redirect_to public_items_path
+      end
     end
   end
   
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,8 +3,8 @@ class CartItem < ApplicationRecord
     belongs_to :customer
     belongs_to :item
     
-    def with_tax_price
-        (price * 1.1).floor
+    def sum_of_price
+        item.with_tax_price * amount
     end
     
     def subtotal
