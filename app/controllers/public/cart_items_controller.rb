b2c67cde831c027737e67deb2d4ee16edf509b85
EC2 Default User <ec2-user@ip-172-31-27-191.ap-northeast-1.compute.internal>
2022-10-23 07:28:33 +0000

    Fix

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -6,7 +6,7 @@ class Public::CartItemsController < ApplicationController
   
   def update
     @cart_item = CartItem.find(params[:id])
-    @cart_item.update(amount: params[:cart_item][:amount].to_i) #to_iで入力した個数を整数に変換
+    @cart_item.update(amount: params[:cart_item][:amount].to_i)
     redirect_to cart_items_path
   end
   
@@ -26,14 +26,14 @@ class Public::CartItemsController < ApplicationController
     @cart_items = CartItem.all
     @cart_item = current_customer.cart_items.new(cart_item_params)
     @update_cart_item = CartItem.find_by(item: @cart_item.item)
-    #カートに入れた商品がカート内商品にあったときは数を足す
-    if @update_cart_item.present? && @cart_item.valid? #present? 存在すればtrue
-       @cart_item.amount += @update_cart_item.amount #カートに追加した個数 + カートにある個数 = 合計個数
-       @update_cart_item.update(amount: @cart_item.amount) #quantitiy(@cart_productのquantity)
-    elsif @cart_item.save #false = カートに同じ商品がない時
+    if @update_cart_item.present? && @cart_item.valid? 
+       @cart_item.amount += @update_cart_item.amount
+       @update_cart_item.update
+       redirect_to cart_items_path
+    elsif @cart_item.save 
       redirect_to cart_items_path
     else
-      @item = @cart_item.item#14行目の@carp_productのproduct_idを探してくる
+      @item = @cart_item.item
       render :index
     end
   end
